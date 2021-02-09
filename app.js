037b3076d2448ee13e166052ec5e3bf22f88d07c
John Manack <jmnck28@gmail.com>
2021-02-09 14:21:54 -0500

    updates app js for routes and updates package json

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const HOSTNAME = '127.0.0.1',
 const express = require('express'),
     app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+const Sequelize = require('sequelize');
+const { User } = require('./models')
+
 const SERVER = HTTP.createServer(app);
 
 SERVER.listen(PORT, HOSTNAME, () => {
@@ -17,3 +23,48 @@ SERVER.listen(PORT, HOSTNAME, () => {
 app.get('/', (req, res) => {
     res.sendStatus(200);
 })
+
+app.get('/users', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+})
+
+app.get('/users/:id', async (req, res) => {
+    const singleUser = await User.findByPk(req.params.id);
+    res.json(singleUser);
+});
+
+app.post('/users', async (req, res) => {
+    const { first_name, last_name, email } = req.body;
+    const newUser = await User.create({
+        first_name,
+        last_name,
+        email
+    });
+
+    res.json({
+        id: newUser.id
+    });
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedUser = await User.destroy({
+        where: {
+            id
+        }
+    });
+    res.json(deletedUser);
+});
+
+app.post('/users/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const updatedUser = await User.update(req.body, {
+        where: {
+            id
+        }
+    });
+
+    res.json(updatedUser)
+});
